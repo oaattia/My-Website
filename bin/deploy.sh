a83ee9f484634b412125f78020d612e048957753
Osama Ahmed <osama.ahmed@its.ws>
2016-12-04 12:27:36 +0000

    add build script and the travis connection

---
+++ bin/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Automated deploy script with Circle CI.
+
+# Exit if any subcommand fails.
+set -e
+
+# build site.
+jekyll build
+
+# Delete and move files.
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+
+# Send latest to production
+rsync -avz ./ --exclude 'bin/deploy.sh' '.git/' $REMOTE:$REMOTE_APP
+
+echo "Deployed Successfully!"
+
+exit 0
---
+++ bin/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Exit if any subcommand fails.
+set -e
+
+gem install jekyll
+gem install jekyll-paginate
+gem install pygments.rb
